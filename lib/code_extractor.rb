c900c5642bf8c932d50499b5f4ebfbbd15a3b91a
Nick LaMuro <nicklamuro@gmail.com>
2019-10-30 17:19:58 -0500

    [CodeExtractor] Convert to module
    This could have been done as part of the "Gemify" effort, but the intent
    of that branch was to make as few changes to the core script before
    starting to make sweeping changes.
    
    By converting `CodeExtractor` to a `module`, that file and
    `lib/code_extractor/version.rb` can be loaded at the same time (yes,
    they couldn't before...), and it is just a better practice for dealing
    with namespaces in general.
    
    The `Runner` class was a necessary side effect, but I think it is mostly
    understandable what it's role is and should be a straight forward
    change.
    
    Fun Fact
    --------
    
    The `attr_reader :extraction` is actually never used.  Kept it as a part
    of `CodeExtractor::Runner`, but really, it is never used.

--- lib/code_extractor.rb
+++ lib/code_extractor.rb
@@ -2,77 +2,80 @@ require 'yaml'
 
 # Class to extract files and folders from a git repository, while maintaining
 # The git history.
-class CodeExtractor
-  attr_reader :extraction
-
-  def self.run
-    new.extract
+module CodeExtractor
+  def run
+    Runner.new.extract
   end
+  module_function :run
 
-  def initialize(extraction = 'extractions.yml')
-    @extraction = YAML.load_file(extraction)
-    @extraction[:upstream_branch] ||= "master"
+  class Runner
+    attr_reader :extraction
 
-    missing = %i[name destination upstream upstream_name extractions].reject { |k| @extraction[k] }
-    raise ArgumentError, "#{missing.map(&:inspect).join(", ")} key(s) missing" if missing.any?
+    def initialize(extraction = 'extractions.yml')
+      @extraction = YAML.load_file(extraction)
+      @extraction[:upstream_branch] ||= "master"
 
-    @extraction[:destination] = File.expand_path(@extraction[:destination])
-  end
+      missing = %i[name destination upstream upstream_name extractions].reject { |k| @extraction[k] }
+      raise ArgumentError, "#{missing.map(&:inspect).join(", ")} key(s) missing" if missing.any?
 
-  def extract
-    puts @extraction
-    clone
-    extract_branch
-    remove_remote
-    remove_tags
-    filter_branch
-  end
-
-  def clone
-    return if Dir.exist?(@extraction[:destination])
-    puts 'Cloning…'
-    system "git clone -o upstream #{@extraction[:upstream]} #{@extraction[:destination]}"
-  end
-
-  def extract_branch
-    puts 'Extracting Branch…'
-    Dir.chdir(@extraction[:destination])
-    branch = "extract_#{@extraction[:name]}"
-    `git checkout #{@extraction[:upstream_branch]}`
-    `git fetch upstream && git rebase upstream/master`
-    if system("git branch | grep #{branch}")
-      `git branch -D #{branch}`
+      @extraction[:destination] = File.expand_path(@extraction[:destination])
     end
-    `git checkout -b #{branch}`
-    extractions = @extraction[:extractions].join(' ')
-    `git rm -r #{extractions}`
-    `git commit -m "Extract #{@extraction[:name]}"`
-  end
 
-  def remove_remote
-    `git remote rm upstream`
-  end
-
-  def remove_tags
-    puts 'removing tags'
-    tags = `git tag`
-    tags.split.each do |tag|
-      puts "Removing tag #{tag}"
-      `git tag -d #{tag}`
+    def extract
+      puts @extraction
+      clone
+      extract_branch
+      remove_remote
+      remove_tags
+      filter_branch
     end
-  end
 
-  def filter_branch
-    extractions = @extraction[:extractions].join(' ')
-    `time git filter-branch --index-filter '
-    git read-tree --empty
-    git reset $GIT_COMMIT -- #{extractions}
-    ' --msg-filter '
-    cat -
-    echo
-    echo
-    echo "(transferred from #{@extraction[:upstream_name]}@$GIT_COMMIT)"
-    ' -- #{@extraction[:upstream_branch]} -- #{extractions}`
+    def clone
+      return if Dir.exist?(@extraction[:destination])
+      puts 'Cloning…'
+      system "git clone -o upstream #{@extraction[:upstream]} #{@extraction[:destination]}"
+    end
+
+    def extract_branch
+      puts 'Extracting Branch…'
+      Dir.chdir(@extraction[:destination])
+      branch = "extract_#{@extraction[:name]}"
+      `git checkout #{@extraction[:upstream_branch]}`
+      `git fetch upstream && git rebase upstream/master`
+      if system("git branch | grep #{branch}")
+        `git branch -D #{branch}`
+      end
+      `git checkout -b #{branch}`
+      extractions = @extraction[:extractions].join(' ')
+      `git rm -r #{extractions}`
+      `git commit -m "Extract #{@extraction[:name]}"`
+    end
+
+    def remove_remote
+      `git remote rm upstream`
+    end
+
+    def remove_tags
+      puts 'removing tags'
+      tags = `git tag`
+      tags.split.each do |tag|
+        puts "Removing tag #{tag}"
+        `git tag -d #{tag}`
+      end
+    end
+
+    def filter_branch
+      extractions = @extraction[:extractions].join(' ')
+      `time git filter-branch --index-filter '
+      git read-tree --empty
+      git reset $GIT_COMMIT -- #{extractions}
+      ' --msg-filter '
+      cat -
+      echo
+      echo
+      echo "(transferred from #{@extraction[:upstream_name]}@$GIT_COMMIT)"
+      ' -- #{@extraction[:upstream_branch]} -- #{extractions}`
+    end
   end
 end
 
