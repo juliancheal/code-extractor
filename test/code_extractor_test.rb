cb508a887d15566c71858f8b5bb84d8b7258ab5f
Nick LaMuro <nicklamuro@gmail.com>
2019-11-14 17:37:05 -0600

    [code_extractor_test] Add .apply_new_commits_on_extracted_repo
    Another helper method that includes boilerplate for updating the newly
    extracted repo with some new commits for use when "unextracting"
    
    Once again, instance variables were used here in a less than graceful
    fashion, but it allowed for moving a bit more code out of the main
    testing method that was mostly trivial.

--- test/code_extractor_test.rb
+++ test/code_extractor_test.rb
@@ -104,22 +104,7 @@ class CodeExtractorTest < CodeExtractor::TestCase
     set_extractions ["foo"]
     run_extraction
     perform_merges_of_extracted_code
-
-    # Run new extraction, with some extra commits added to the new repo that
-    # has been extracted previously
-
-    new_upstream_dir       = File.join @sandbox_dir, "new_upstream.git"
-    cloned_extractions_dir = File.join @sandbox_dir, "cloned_extractions.git"
-
-    CodeExtractor::TestRepo.clone_at extracted_dir, cloned_extractions_dir do
-      checkout_b 'master', 'origin/master'
-
-      update_file "foo/bar", "Updated Bar Content"
-      commit "update bar content"
-
-      update_file "foo/baz", "Baz Content"
-      commit "add new baz"
-    end
+    apply_new_commits_on_extracted_repo
 
     # Update the configuration for the (second) extraction
 
@@ -130,7 +115,7 @@ class CodeExtractorTest < CodeExtractor::TestCase
     extractions_hash[:target_name]        = "MyOrg/extracted_repo"
     extractions_hash[:target_remote]      = @bare_repo_dir
     extractions_hash[:target_base_branch] = "master"
-    extractions_hash[:upstream]           = cloned_extractions_dir
+    extractions_hash[:upstream]           = @cloned_extractions_dir
     extractions_hash[:upstream_name]      = "MyOrg/repo"
     extractions_hash[:extra_cmds]         = [
       "mkdir lib",
@@ -139,9 +124,12 @@ class CodeExtractorTest < CodeExtractor::TestCase
       "git commit -m 'Move foo/ into lib/'"
     ]
 
-    set_destination_dir new_upstream_dir
+    set_destination_dir @new_upstream_dir
 
-    # The run the actual extraction we are testing
+    # Run new extraction, with some extra commits added to the new repo that
+    # has been extracted previously
+    #
+    # This next line will run the actual extraction we are testing
     #
     # aka:  updated commits and puts 'lib/foo' back into the original repo
     run_extraction
@@ -224,5 +212,20 @@ class CodeExtractorTest < CodeExtractor::TestCase
     )
 
     original_repo.remotes['origin'].push [original_repo.head.name]
+  end
+
+  def apply_new_commits_on_extracted_repo
+    @new_upstream_dir       = File.join @sandbox_dir, "new_upstream.git"
+    @cloned_extractions_dir = File.join @sandbox_dir, "cloned_extractions.git"
+
+    CodeExtractor::TestRepo.clone_at extracted_dir, @cloned_extractions_dir do
+      checkout_b 'master', 'origin/master'
+
+      update_file "foo/bar", "Updated Bar Content"
+      commit "update bar content"
+
+      update_file "foo/baz", "Baz Content"
+      commit "add new baz"
+    end
   end
 end
